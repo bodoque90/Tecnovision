470bbce05fa295364295b82c35e263d75a4c56a3
bodoque90 <jesuuuus490gmail.com>
2025-11-10 15:20:24 -0300

    prueba de conexion

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors'); // Importamos cors
+
 
 const db = require('./db');
 const app = express();
@@ -9,9 +12,20 @@ const port = 3001; // Usamos un puerto diferente al de React (que usa el 3000)
 app.use(cors());
 
 // Nuestra ruta de API
-app.get('/api/mensaje', (req, res) => {
-  // Enviamos un JSON como respuesta
-  res.json({ message: "¡Hola! Este mensaje viene del servidor Node.js" });
+app.get('/api/mensaje', async (req, res) => {
+  try {
+    // Usamos la función 'query' que exportamos
+    const result = await db.query('SELECT NOW()');
+    const horaDeLaBD = result.rows[0].now;
+
+    res.json({
+      message: `¡Conexión a BD exitosa! Hora de la BD: ${horaDeLaBD}`
+    });
+
+  } catch (error) {
+    console.error('Error al consultar la base de datos', error);
+    res.status(500).json({ message: 'Error conectando a la BD' });
+  }
 });
 
 // Iniciamos el servidor
